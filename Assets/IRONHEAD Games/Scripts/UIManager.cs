ef9aa007eb337574b8b544804da7ee9e8458a143
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2021-05-25 20:18:16 -0400

    add menu UI interaction

---
+++ Assets/IRONHEAD Games/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject UI_VRMenuGameobject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UI_VRMenuGameobject.SetActive(false);
+    }
+
+    public void OnWorldsButtonClicked(){
+        Debug.Log("Worlds button is clicked!");
+    }
+    public void OnGoHomedButtonClicked(){
+        Debug.Log("Go Home button is clicked!");
+    }
+    public void OnChangeAvatarButtonClicked(){
+        Debug.Log("Change Avatar button is clicked!");
+    }
+}
